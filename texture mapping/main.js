ee1bd8e5f7caff4c80116e9c25c5b9cf90df24a8
Shubhanshu1902 <shubhanshu1902@gmail.com>
2023-05-05 11:37:43 +0530

    final

--- texture mapping/main.js
+++ texture mapping/main.js
@@ -3,6 +3,7 @@ import "./style.css";
 import { GUI } from "dat.gui";
 import { v1 } from "./vertexShader";
 import { v1f, v2f } from "./fragmentShader";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Creating scene
 const scene = new THREE.Scene();
@@ -14,6 +15,17 @@ const constants = {
     height: window.innerHeight,
 };
 
+// Renderer
+const canvas = document.querySelector(".webgl");
+console.log(canvas);
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+});
+renderer.setSize(constants.width, constants.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.render(scene, camera);
+
 // Camera
 const camera = new THREE.PerspectiveCamera(
     60,
@@ -21,7 +33,8 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     100
 );
-camera.position.z = 20;
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set(0, 2, 6)
 scene.add(camera);
 
 let vals = {
@@ -32,18 +45,13 @@ let vals = {
 let t1 = new THREE.TextureLoader().load('./uv_mapper.jpg')
 
 let uniforms = {
-    c1: { value: new THREE.Color(0xff0000) },
-    c2: { value: new THREE.Color(0x00ff00) },
-    c3: { value: new THREE.Color(0x0000ff) },
-    c4: { value: new THREE.Color(0x00ffff) },
     t1: {value: t1}
 };
 
-const getObject = (object, mapping, uniforms) => {
-    let geometry;
-    if (object === "Cube") geometry = new THREE.BoxGeometry(3, 3, 3);
-    else if (object === "Sphere") geometry = new THREE.SphereGeometry(3);
-    else geometry = new THREE.CylinderGeometry(4, 4, 10);
+function addObjects(mapping){
+    const cubeGeo = new THREE.BoxGeometry(1, 1, 1);
+    const sphereGeo = new THREE.SphereGeometry(1);
+    const CylinderGeo = new THREE.CylinderGeometry(0.5, 0.5, 1);
 
     let fshader = v1f;
     if (mapping === "Cylindrical") {
@@ -56,43 +64,37 @@ const getObject = (object, mapping, uniforms) => {
         fragmentShader: fshader,
     });
 
-    const mesh = new THREE.Mesh(geometry, material);
-    return mesh;
-};
+    const cube = new THREE.Mesh(cubeGeo, material);
+    cube.position.set(0, 0.5, 0)
+    const sphere = new THREE.Mesh(sphereGeo, material);
+    sphere.position.set(-2, 0.5, 0);
+    const cylinder = new THREE.Mesh(CylinderGeo, material);
+    cylinder.position.set(2, 0.5, 0)
 
-let mesh = getObject(vals.Object, vals.Mapping, uniforms);
-scene.add(mesh);
+    return [cube, sphere, cylinder]
+}
+var meshes = addObjects(vals.Mapping)
+for(let mesh of meshes)
+    scene.add(mesh);
 
 const gui = new GUI();
 const f1 = gui.addFolder("Texture Mapping");
-f1.add(vals, "Object", ["Sphere", "Cube", "Cylinder"]).onChange(() => {
-    scene.remove(mesh);
-    mesh = getObject(vals.Object, vals.Mapping, uniforms);
-    scene.add(mesh);
-});
+
 
 f1.add(vals, "Mapping", ["Spherical", "Cylindrical"]).onChange(() => {
-    scene.remove(mesh);
-    mesh = getObject(vals.Object, vals.Mapping, uniforms);
-    scene.add(mesh);
+    for(let mesh of meshes)
+        scene.remove(mesh);
+    meshes = addObjects(vals.Mapping);
+    for(let mesh of meshes)
+        scene.add(mesh);
 });
 
-
-// Renderer
-const canvas = document.querySelector(".webgl");
-console.log(canvas);
-const renderer = new THREE.WebGLRenderer({
-    canvas,
-    antialias: true,
-});
-renderer.setSize(constants.width, constants.height);
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.render(scene, camera);
 
 // Animation
 const loop = () => {
-    mesh.rotation.x += 0.005;
-    mesh.rotation.z += 0.005;
+    controls.update()
+    // mesh.rotation.x += 0.005;
+    // mesh.rotation.z += 0.005;
     renderer.render(scene, camera);
     window.requestAnimationFrame(loop);
 };
